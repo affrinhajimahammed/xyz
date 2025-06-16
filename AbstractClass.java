5576b8ecd88a20aea71062327733f94eefd313ba
Affrin Hajimahammed <affrinhajimahammed@gmail.com>
2025-06-16 11:52:27 +0530

    Update AbstractClass.java

--- AbstractClass.java
+++ AbstractClass.java
@@ -1,11 +1,18 @@
 package xyz;
 abstract class machine{
 	abstract void ATM();
-	public void ATM2
+	public void ATM2(){
+System.out.println("ATM USING JAVA WITH PHP");
 	
 }
+	class Bank extends Machine{
+		System.out.println("ATM MACHINE  USINH JAVA");
 
 public class AbstractClass {
+	public static void main(String[] args){
+		Bank b = new ;
+		b.ATM();
+		b.ATM2();
 	
 
 }
